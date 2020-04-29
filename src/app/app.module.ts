19132fc745a09fe3cffd28e2394080ec8c90341f
c1480057 <c14180057@john.petra.ac.id>
2020-04-29 07:50:10 +0700

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import {Routes,RouterModule} from '@angular/router';
+const ROUTES:Routes=[
+  {
+    path:'page1',component:Page1Component
+  }
+  ,
+  {
+    path:'edit',component:Page2Component
+  }
+]
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, Page1Component, Page2Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
