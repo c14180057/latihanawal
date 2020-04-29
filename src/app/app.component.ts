286942f53a81a34fd053a7f97d8a186c8ed2865c
c1480057 <c14180057@john.petra.ac.id>
2020-04-29 07:53:52 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+   constructor (private router : Router) {}
+  page1(){
+    this.router.navigate(['/page1']);
+
+  }
+  
+  page2()
+  {
+      this.router.navigate(['/page2']);
+  }
 }
