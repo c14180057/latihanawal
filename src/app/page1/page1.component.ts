d4a0f53fae73e6b661ebdd88734658bdf133d3e4
c1480057 <c14180057@john.petra.ac.id>
2020-04-29 08:17:57 +0700

    2 files modified

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -11,5 +11,59 @@ export class Page1Component implements OnInit {
   angka=""
   ngOnInit() {
   }
-  
+  test(angka)
+  {
+    if(angka=="1")
+    {
+
+    }
+    else if(angka=="2")
+    {
+
+    }
+    else if(angka=="3")
+    {
+      
+    }
+      else if(angka=="4")
+    {
+      
+    }
+      else if(angka=="5")
+    {
+      
+    }
+    else if(angka=="6")
+    {
+      
+    }
+    else if(angka=="7")
+    {
+      
+    }
+    else if(angka=="8")
+    {
+      
+    }
+    else if(angka=="9")
+    {
+      
+    }
+    else if(angka=="min")
+    {
+      
+    }
+    else if(angka=="plus")
+    {
+      
+    }
+    else if(angka=="mul")
+    {
+      
+    }
+    else if(angka=="equal")
+    {
+      
+    }
+  }
 }
