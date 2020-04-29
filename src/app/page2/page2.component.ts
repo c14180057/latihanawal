cb8f496803bbac12c122e1f33613c6c1cc1d336f
c1480057 <c14180057@john.petra.ac.id>
2020-04-29 09:01:30 +0700

    2 files modified

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Page1Component } from '../page1/page1.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
@@ -7,10 +8,13 @@ import { Page1Component } from '../page1/page1.component';
 })
 export class Page2Component implements OnInit {
 
-  constructor(private pageComponent: Page1Component ) { }
+  constructor(private pageComponent: Page1Component,private router : Router ) { }
   res;
   ngOnInit() {
-    this.res=this.pageComponent.getresult()
+    // this.res=this.pageComponent.getresult()
   }
-
+  back()
+  {
+     this.router.navigate(['/page1']);
+  }
 }
