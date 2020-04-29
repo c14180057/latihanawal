c411f1cbfe573f3d916bc67df9e8ba59a3ff7fe0
c1480057 <c14180057@john.petra.ac.id>
2020-04-29 08:20:31 +0700

    2 files modified

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -8,62 +8,62 @@ import { Component, OnInit } from '@angular/core';
 export class Page1Component implements OnInit {
 
   constructor() { }
-  angka=""
+  text=""
   ngOnInit() {
   }
   test(angka)
   {
     if(angka=="1")
     {
-
+      this.text=this.text+"1"
     }
     else if(angka=="2")
     {
-
+      this.text=this.text+"2"
     }
     else if(angka=="3")
     {
-      
+      this.text=this.text+"3"
     }
       else if(angka=="4")
     {
-      
+      this.text=this.text+"4"
     }
       else if(angka=="5")
     {
-      
+      this.text=this.text+"5"
     }
     else if(angka=="6")
     {
-      
+      this.text=this.text+"6"
     }
     else if(angka=="7")
     {
-      
+      this.text=this.text+"7"
     }
     else if(angka=="8")
     {
-      
+      this.text=this.text+"8"
     }
     else if(angka=="9")
     {
-      
+      this.text=this.text+"9"
     }
     else if(angka=="min")
     {
-      
+      this.text=this.text+"-"
     }
     else if(angka=="plus")
     {
-      
+      this.text=this.text+"+"
     }
     else if(angka=="mul")
     {
-      
+      this.text=this.text+"*"
     }
     else if(angka=="equal")
     {
-      
+      this.text=this.text+"="
     }
   }
 }
